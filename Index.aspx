60ed84fe462fbff61d8f4c478ab2fa1ca8dc3d93
MarBlueish <6958234@formacao.iefp.pt>
2024-01-26 18:46:45 +0000

    Add project files.

---
+++ Index.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="EvaluationActiv.Index" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="pt">
+<head runat="server">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Página com Tema Escuro</title>
+    <style>
+        body {
+            background-color: #1f1f1f;
+            color: #fff;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            background-color: #333;
+            padding: 10px;
+            text-align: center;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        footer {
+            background-color: #333;
+            padding: 10px;
+            text-align: center;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Minha Página com Tema Escuro</h1>
+    </header>
+
+    <section>
+        <form id ="form1" runat="server">
+            <div>
+                &nbsp;<img width="200" src="bike.jpeg" /><br /> <br />
+                <asp:Button id="B_ins" runat="server" text="Inserir" OnClick="B_ins_Click" />&nbsp;
+                <asp:Button id="B_list" runat="server" Text="Listar" OnClick="B_list_Click" />&nbsp;
+                <asp:Button ID="B_alt" runat="server" Text="Alterar/Eliminar" OnClick="B_alt_Click" />&nbsp;
+            </div>
+            </form>
+        <p></p>
+    </section>
+
+    <footer>
+        <p>Bikes UTD</p>
+    </footer>
+</body>
+</html>
---
+++ Index.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+// ver linha 31
+
+namespace EvaluationActiv
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void B_alt_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection con;
+            // veja a imagem abaixo para saber onde vai buscar o caminho da conexão
+            connetionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\EvaluationActiv\App_Data\dtbase.mdf;Integrated Security=True";
+            con = new SqlConnection(connetionString);
+            con.Open();
+            // Response.Write("Ligado com sucesso!");
+            // a linha acima utilizo apenas para ver se a conexão é feita com sucesso
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+            //sql = "delete motadb where id=" + txt_id_elem.Text; // necessario saber o que eliminar, voltar a esta pg
+            // Response.Write(sql);
+            // a linha acima serve para verem o que esta a ser executado no vosso sql
+            command = new SqlCommand(sql, con);
+            adapter.DeleteCommand = new SqlCommand(sql, con);
+            adapter.DeleteCommand.ExecuteNonQuery();
+            command.Dispose();
+            con.Close();
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "Dados", "alert('Registo apagado com sucesso');window.location='Index.aspx';", true);
+
+        }
+
+        protected void B_list_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void B_ins_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
